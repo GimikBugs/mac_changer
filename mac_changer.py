468d6467457367f1e4911f94710c63d2b948585f
Gimik <gimikbugs@gmail.com>
2019-12-23 22:35:50 -0600

    Starting to add command line arguments instead of user input

--- mac_changer.py
+++ mac_changer.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
+
+# Implementing OptionParser class for parser object instance
+parser = optparse.OptionParser()
+# New command line arguments
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change the MAC address of..")
+parser.add_option("-m", "--mac", dest="mac", help="New MAC address..")
+
+parser.parse_args()
 
 interface = input("Interface > ")
-new_mac = input("New MAC address > ")
+mac = input("New MAC address > ")
 
-print("[+] Changing MAC address for " + interface + " to " + new_mac + "...")
+print("[+] Changing MAC address for " + interface + " to " + mac + "...")
 
 subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "hw", "ether", mac])
 subprocess.call(["ifconfig", interface, "up"])
