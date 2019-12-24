ef2167fd6fd4e4badbc121d88945d899db38747d
Gimik <gimikbugs@gmail.com>
2019-12-23 22:04:08 -0600

    Adding user input for interface and MAC address

--- mac_changer.py
+++ mac_changer.py
@@ -2,13 +2,12 @@
 
 import subprocess
 
-interface = "wlan0"
-new_mac = "00:11:22:33:44:55"
+interface = input("Interface > ")
+new_mac = input("New MAC address > ")
 
-print("[+] Changing MAC for " + interface)
+print("[+] Changing MAC address for " + interface + " to " + new_mac + "...")
 
 subprocess.call("sudo ifconfig " + interface + " down", shell=True)
 subprocess.call("sudo ifconfig " + interface + " hw ether " + new_mac, shell=True)
 subprocess.call("sudo ifconfig wlan0 up", shell=True)
 
-print("[+] " + interface + " has been changed to " + new_mac)
