a7cf7e9a8517b1af9883d5bec1c24fd7d67c1aa0
Gimik <gimikbugs@gmail.com>
2019-12-23 21:48:40 -0600

    Adding readme

--- mac_changer.py
+++ mac_changer.py
@@ -2,4 +2,13 @@
 
 import subprocess
 
-subprocess.call("ifconfig", shell=True)
+interface = "wlan0"
+new_mac = "00:11:22:33:44:55"
+
+print("[+] Changing MAC for " + interface)
+
+subprocess.call("sudo ifconfig " + interface + " down", shell=True)
+subprocess.call("sudo ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("sudo ifconfig wlan0 up", shell=True)
+
+print("[+] " + interface + " has been changed to " + new_mac)
